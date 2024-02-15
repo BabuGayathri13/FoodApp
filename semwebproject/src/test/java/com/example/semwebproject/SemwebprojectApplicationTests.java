f3fa0fe83c69153f60f2d058bc669d5a4fe48985
Gayathri <Gayathri.mathan@gmail.com>
2024-02-15 13:41:19 +0000

    intial commit of Springboot mavenProject file

---
+++ semwebproject/src/test/java/com/example/semwebproject/SemwebprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.semwebproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemwebprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
