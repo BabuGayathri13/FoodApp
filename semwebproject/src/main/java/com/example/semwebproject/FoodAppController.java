4bb7cf85e700e0220319ef24b4a11f877f7d1876
Pradeep Babu <pradeep.babu@ASSUMEOBTAINBLAME.workdayinternal.com>
2024-03-02 14:23:44 +0000

    Filter based on cuisines

--- semwebproject/src/main/java/com/example/semwebproject/FoodAppController.java
+++ semwebproject/src/main/java/com/example/semwebproject/FoodAppController.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +19,18 @@ public class FoodAppController {
     @GetMapping("allItems")
     public ArrayList<JdbcJava> getAllItems() throws SQLException {
         return select.getAllItems();
+    }
+
+    @GetMapping("allItems/{cuisine}")
+    public ArrayList<JdbcJava> getCuisine(@PathVariable("cuisine") final String cuisine) throws SQLException {
+        final ArrayList<JdbcJava> allItems = select.getAllItems();
+        final ArrayList<JdbcJava> filteredCuisines = new ArrayList<>();
+        for (final JdbcJava item : allItems) {
+            if(cuisine.equalsIgnoreCase(item.getCuisine())) {
+                filteredCuisines.add(item);
+            }
+        }
+        return filteredCuisines;
     }
 
     @GetMapping("ping")
