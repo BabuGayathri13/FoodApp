ddf1fd823c6561d22f7fed07d3de1908ccdee874
Gayathri <Gayathri.mathan@gmail.com>
2024-03-03 12:30:12 +0000

    Updated source code

--- semwebproject/src/main/java/com/example/semwebproject/HomeController.java
+++ semwebproject/src/main/java/com/example/semwebproject/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-	@GetMapping("")
+	@GetMapping("/")
 	public String index() {
 		return "index"; // This corresponds to "index.html" in the "static" directory
 	}
